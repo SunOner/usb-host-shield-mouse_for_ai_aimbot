27916edbe06e922b2172572bafe9098193ff6d77
SunOne <44489132+SunOner@users.noreply.github.com>
2024-03-24 16:20:17 +0300

    Update hidmousereport.ino

--- hidmousereport/hidmousereport.ino
+++ hidmousereport/hidmousereport.ino
@@ -22,66 +22,106 @@ void setup() {
 
 void loop() {
   	Usb.Task();
+
+    if (Serial.available() > 0)
+    {
+        String command = Serial.readStringUntil('\n');
+        ParseSerialCommand(command);
+    }
+}
+
+void ParseSerialCommand(const String& command)
+{
+    if (command == "c")
+    {
+        Mouse.click();
+    }
+    else if (command == "r")
+    {
+        Mouse.release();
+    }
+    else if (command == "p")
+    {
+        Mouse.press();
+    }
+    else if (command.startsWith("m"))
+    {
+        ExecuteMouseMoveCommand(command);
+    }
+}
+
+void ExecuteMouseMoveCommand(const String& command)
+{
+    String moveCommand = command;
+    moveCommand.replace("m", "");
+
+    int commaIndex = moveCommand.indexOf(',');
+    int x = moveCommand.substring(0, commaIndex).toInt();
+    int y = moveCommand.substring(commaIndex + 1).toInt();
+
+    Mouse.move(x, y, 0);
 }
 
 void onButtonDown(uint16_t buttonId) {
 	Mouse.press(buttonId);
-	Serial.print("Button ");
-	switch (buttonId) {
+	// Serial.print("Button ");
+	switch (buttonId)
+  {
 		case MOUSE_LEFT:
-			Serial.print("MOUSE_LEFT");
+			Mouse.press(1);
 			break;
 
 		case MOUSE_RIGHT:
-			Serial.print("MOUSE_RIGHT");
+			Mouse.press(2);
 			break;
 		
 		case MOUSE_MIDDLE:
-			Serial.print("MOUSE_MIDDLE");
+			Mouse.press(4);
 			break;
 
 		case MOUSE_BUTTON4:
-			Serial.print("MOUSE_BUTTON4");
+			// Serial.print("MOUSE_BUTTON4");
 			break;
 
 		case MOUSE_BUTTON5:
-			Serial.print("MOUSE_BUTTON5");
+			// Serial.print("MOUSE_BUTTON5");
 			break;
 		default:
-			Serial.print("OTHER_BUTTON");
+			// Serial.print("OTHER_BUTTON");
 			break;
 	}
-	Serial.println(" pressed");
+	// Serial.println(" pressed");
 }
 
 void onButtonUp(uint16_t buttonId) {
 	Mouse.release(buttonId);
-	Serial.print("Button ");
-	switch (buttonId) {
+	// Serial.print("Button ");
+	switch (buttonId)
+  {
 		case MOUSE_LEFT:
-			Serial.print("MOUSE_LEFT");
+			Mouse.release(1);
 			break;
 
 		case MOUSE_RIGHT:
-			Serial.print("MOUSE_RIGHT");
+			Mouse.release(2);
 			break;
 		
 		case MOUSE_MIDDLE:
-			Serial.print("MOUSE_MIDDLE");
+			Mouse.release(4);
 			break;
 
 		case MOUSE_BUTTON4:
-			Serial.print("MOUSE_BUTTON4");
+			// Serial.print("MOUSE_BUTTON4");
 			break;
 
 		case MOUSE_BUTTON5:
-			Serial.print("MOUSE_BUTTON5");
+			// Serial.print("MOUSE_BUTTON5");
 			break;
 		default:
-			Serial.print("OTHER_BUTTON");
+			// Serial.print("OTHER_BUTTON");
 			break;
 	}
-	Serial.println(" released");
+	// Serial.println(" released");
 }
 
 void onTiltPress(int8_t tiltValue) {
@@ -90,11 +130,5 @@ void onTiltPress(int8_t tiltValue) {
 }
 
 void onMouseMove(int8_t xMovement, int8_t yMovement, int8_t scrollValue) {
-	Mouse.move(xMovement, yMovement, scrollValue);
-	Serial.print("Mouse moved:\t");
-	Serial.print(xMovement);
-	Serial.print("\t");
-	Serial.print(yMovement);
-	Serial.print("\t");
-	Serial.println(scrollValue);
+  Mouse.move(xMovement, yMovement, scrollValue);
 }
